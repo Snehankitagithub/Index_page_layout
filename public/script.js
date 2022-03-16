89702784d8fb3cb37125c56103c62859e26be249
Snehankita Osaramkar <sssosaramkar@gmail.com>
2022-03-17 01:15:39 +0530

    commit

--- public/script.js
+++ public/script.js
@@ -161,28 +161,3 @@ document.querySelectorAll(".stories-inner-tab>*").forEach((item)=>{
 
 
 
-const hamburger = document.querySelector('.menu-nav  .hamburger');
-const mobile_menu = document.querySelector('.menu-nav  ul');
-const menu_item = document.querySelectorAll('.menu-nav  ul li a');
-const header = document.querySelector('.menu-nav.container');
-
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
-
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
